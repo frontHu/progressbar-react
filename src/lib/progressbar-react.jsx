c547b83a8653713299b783f638f4408674be9f84
hzw <405163706@qq.com>
2018-08-24 17:13:45 +0800

    修改按钮超出问题

--- src/lib/progressbar-react.jsx
+++ src/lib/progressbar-react.jsx
@@ -5,9 +5,7 @@ class ReactProgress extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      PERCENT: this.props.percent, //进度
-      BAR_LEFT: 0, //进度条的百分比
-      BTN_LEFT: 0 //拖动按钮的百分比
+      PERCENT: this.props.percent
     }
     this.progressContainer = null;
     this.progressBtn = null;
@@ -15,13 +13,18 @@ class ReactProgress extends Component {
     this.progressBtnWidth = 0;
     this.propgressContainerWidth = 0;
     this.color = this.props.color ? this.props.color : '#1890ff';
-    this.seekTo = this.props.seekTo ? this.props.seekTo : ()=>{}
+    this.seekTo = this.props.seekTo ? this.props.seekTo : ()=>{};
   }
 
   componentDidMount() {
     this.init();
     this.dragMove();
     this.clickMove();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let { percent } = nextProps;
+    this.setState({ PERCENT: percent });
   }
   
   //初始化
@@ -30,16 +33,6 @@ class ReactProgress extends Component {
     this.progressBtn = document.querySelector('#btn-progress');
     this.progressBtnWidth = this.progressBtn.offsetWidth;
     this.progressContainerWidth = this.progressContainer.offsetWidth;
-  }
-
-  //设置进度
-  setPercent(L) {
-    this.setState({
-      PERCENT: Math.floor(Math.round(L * 100 / this.progressContainerWidth)), 
-      BAR_LEFT: L
-    }, () => {
-      this.seekTo();
-    });
   }
 
   //拖动进度条
@@ -58,6 +51,7 @@ class ReactProgress extends Component {
         } else if (L >= this.progressContainerWidth) {
           L = this.progressContainerWidth;
         }
+        console.log(L * 100 / this.progressContainerWidth, 'l')
         this.setPercent(L);
       }
 
@@ -72,22 +66,31 @@ class ReactProgress extends Component {
 
   //点击进度条
   clickMove() {
-    this.progressContainer.onclick = (e) => {
+    this.progressContainer.onmousedown = (e) => {
       e.stopPropagation();
       let L = e.clientX - this.getOffsetLeft(this.progressContainer);
       if(L <= 0) {
         L = 0;
-      }else if(L >= this.progressContainerWidth-this.progressBtnWidth) {
-        L = this.progressContainerWidth-this.progressBtnWidth;
+      }else if(L >= this.progressContainerWidth) {
+        L = this.progressContainerWidth;
       } 
       this.setPercent(L);
     }
   }
 
+  //设置进度
+  setPercent(L) {
+    this.setState({
+      PERCENT: (L * 100 / this.progressContainerWidth), 
+    }, () => {
+      this.seekTo(this.state.PERCENT);
+    });
+  }
+
   //获取offsetLeft
   getOffsetLeft(target) {
     let left = 0;
-    if(target.offsetParent) {
+    while(target.offsetParent) {
       left += target.offsetLeft;
       target = target.offsetParent;
     }
@@ -96,31 +99,18 @@ class ReactProgress extends Component {
 
 
   render() {
-    console.log(this.state.PERCENT)
     return (
       <div className="react-progress">
         <div id="outer-progress" className="react-outer-progress" >
-          <span
+          <div
             id='inner-progress'
             className="react-inner-progress"
-            style={{
-              width: `${this.state.PERCENT}%`,
-              backgroundColor: this.color
-            }}
+            style={{ width: `${this.state.PERCENT}%`, backgroundColor: this.color }}
           >
-          </span>
-          <span
-              id='btn-progress'
-              className="react-inner-progress-btn"
-              style={{left: `${this.state.PERCENT}%`, marginLeft: this.state.PERCENT===100?'-6px':''}}
-            >
-              <span 
-                className="react-inner-progress-btn-inner"
-                style={{
-                  backgroundColor: this.color
-                }}
-              ></span>
+            <span id='btn-progress' className="react-inner-progress-btn">
+              <span className="react-inner-progress-btn-inner" style={{ backgroundColor: this.color }}></span>
             </span>
+          </div>
         </div>
       </div>
     )
